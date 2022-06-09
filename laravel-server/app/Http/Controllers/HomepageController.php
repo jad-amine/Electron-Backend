38305f9693360961c3dca87df6f771103a36c3e0
jad amine <jade1992amine@gmail.com>
2022-06-09 07:56:40 +0300

    Added Electron and Front End folder

---
+++ laravel-server/app/Http/Controllers/HomepageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomepageController extends Controller{
+    
+    public function getSomeItems(){
+        return response()->json([
+            // check localstorage for token 
+            // query some items 
+            "status" => "Success",
+            "Items page" => 'some items + user name'
+        ], 200);
+    }
+}
---
+++ laravel-server/routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\AuthController;
+
+// For JWT controller 
+// Route::controller(AuthController::class)->group(function () {
+
+Route::group(['prefix' => 'v1'], function(){
+  //open API
+  Route::get('/', [HomepageController::class, 'getSomeItems']);
+  
+    // Users Routes
+  Route::group(['prefix' => 'user'], function(){
+      //open API
+      Route::get('/login', [UserController::class, 'login']);  
+      Route::post('/signUp', [UserController::class, 'signUp']);
+      //should be only accessible by the admin (need authorization)
+      Route::get('/{id?}', [UserController::class, 'getAllUsers']);
+    });
+
+  Route::group(['prefix' => 'item'], function(){
+    //should be only accessible by the admin (need authorization)
+    Route::post('/addItem', [ItemController::class, 'addItem']);
+    //open API
+    Route::get('/offers', [ItemController::class, 'getOffers']);
+    //open API
+    Route::get('/{id?}', [ItemController::class, 'getItem']);
+  });
+
+});
+
+
